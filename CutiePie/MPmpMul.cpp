ab61c125d744fb1fdb3dad43178b1dbbdc9079e3
unknown <qiudavy@mail.ustc.edu.cn>
2014-09-08 15:00:33 +0800

    Finish kara. 1M ~ 3m10s

--- CutiePie/MPmpMul.cpp
+++ CutiePie/MPmpMul.cpp
@@ -1,23 +1,21 @@
 #include "stdafx.h"
 
 #include "MultiPrecision.h"
-#include "MPmpMul.h"
 
 #include <algorithm>
-#include <thread>
 
 namespace MP
 {
-	MPfloat MPfloat::operator*(const MPfloat& mpf)
+	void MpMul(MPfloat& res, const MPfloat& mpf1, const MPfloat& mpf2)
 	{
-		LPuint *a1 = m_data.A, *a2 = mpf.m_data.A;
-		int l1 = m_data.len, l2 = mpf.m_data.len;
-		int e1 = m_data.exp, e2 = mpf.m_data.exp;
+		LPuint *a1 = mpf1.m_data.A, *a2 = mpf2.m_data.A;
+		int l1 = mpf1.m_data.len, l2 = mpf2.m_data.len;
+		int e1 = mpf1.m_data.exp, e2 = mpf2.m_data.exp;
 
 		if (std::min(l1, l2) > 100)
 		{
 			int k = std::min(l1, l2) / 2;
-			MPfloat a11(*this), a12(*this), a21(mpf), a22(mpf);
+			MPfloat a11(mpf1), a12(mpf1), a21(mpf2), a22(mpf2);
 			a11.m_data.len = k;
 			a11.m_data.exp = 0;
 			a12.m_data.len = l1 - k;
@@ -30,29 +28,14 @@ namespace MP
 			a22.m_data.exp = 0;
 			a22.m_data.A = a22.m_data.A + k;
 
-
-			MPfloat Z0, Z1, Z2;// Z0 = a11 * a21;// Z2 = a12 * a22;
-			if (std::min(l1, l2) > 1000)
-			{
-				std::thread t1(MpMul, std::ref<MPfloat>(Z0), a11, a21);
-				std::thread t2(MpMul, std::ref<MPfloat>(Z2), a12, a22);
-				Z1 = (a11 + a12) * (a21 + a22);
-				t1.join();
-				t2.join();
-			}
-			else
-			{
-				MpMul(Z0, a11, a21);
-				MpMul(Z2, a12, a22);
-				Z1 = (a11 + a12) * (a21 + a22);
-			}
-
-			Z1 = Z1 - Z0 - Z2;
+			MPfloat Z0 = a11 * a21;
+			MPfloat Z2 = a12 * a22;
+			MPfloat Z1 = (a11 + a12) * (a21 + a22) - Z0 - Z2;
 
 			Z0.m_data.exp += e1 + e2;
 			Z1.m_data.exp += e1 + e2 + k;
 			Z2.m_data.exp += e1 + e2 + k + k;
-			return Z0 + Z1 + Z2;
+			res = Z0 + Z1 + Z2;
 		}
 		else
 		{
@@ -96,12 +79,7 @@ namespace MP
 				out.m_data.len++;
 				a3[out.m_data.len - 1] = sup2;
 			}
-			return out;
+			res = out;
 		}
-	}
-	MPfloat MPfloat::operator*(const LPuint& lpf)
-	{
-		MPfloat temp(lpf, 0);
-		return operator*(temp);
 	}
 }
---
+++ CutiePie/MPmpMul.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "MultiPrecision.h"
+
+namespace MP
+{
+	void MpMul(MPfloat& res, const MPfloat& mpf1, const MPfloat& mpf2);
+}
